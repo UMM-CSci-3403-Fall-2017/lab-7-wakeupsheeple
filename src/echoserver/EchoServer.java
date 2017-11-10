b94b089e1914d80761ffca496f627f61663383df
smil1201 <mill5978@morris.umn.edu>
2017-11-09 19:44:51 -0600

    Wrote body of server file, tests pass.

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -7,23 +7,56 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-	public static final int PORT_NUMBER = 6013;
+	public static final int PORT_NUMBER = 6014;
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		EchoServer server = new EchoServer();
 		server.start();
 	}
 
-	private void start() throws IOException, InterruptedException {
-		ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
-		while (true) {
-			Socket socket = serverSocket.accept();
+	private void start() throws IOException, InterruptedException {	
+		try(ServerSocket serverSocket = new ServerSocket(PORT_NUMBER)){				
+			System.out.println("listening...");
+			while (true) {	
+				multiThread m = new multiThread(serverSocket);
+			        m.start();	
+			}
+		} catch(IOException e) {
+			System.out.println("ServerSocket creation failed"); 
+
+		}
+	}
+}
+
+class multiThread extends Thread {
+	private Thread thread;
+	private Socket socket;
+
+	multiThread(ServerSocket s){
+		try{
+			System.out.println("Thread created...");
+			socket = s.accept();
+		} catch (IOException e){
+			System.out.println("multiThread exception");
+		}
+
+	}
+
+	public void run(){
+		try{
 			InputStream inputStream = socket.getInputStream();
 			OutputStream outputStream = socket.getOutputStream();
 			int b;
-			while ((b = inputStream.read()) != -1) {
+			while((b = inputStream.read()) != -1){
 				outputStream.write(b);
 			}
+		} catch (IOException e){
+			System.out.println("run exception");
 		}
+	}
+
+	public void start(){
+		thread = new Thread(this);
+		thread.start();
 	}
 }

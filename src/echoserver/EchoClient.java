c894323bb7e5aa8c499e2464a37f258f543621a7
smil1201 <mill5978@morris.umn.edu>
2017-11-09 19:45:45 -0600

    Wrote body of client, still needs flushing handled.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -6,23 +6,83 @@ import java.io.OutputStream;
 import java.net.Socket;
 
 public class EchoClient {
-	public static final int PORT_NUMBER = 6013;
+	public static final int PORT_NUMBER = 6014;
+	public static Socket socket;
 
 	public static void main(String[] args) throws IOException {
 		EchoClient client = new EchoClient();
+		socket = new Socket("localhost", PORT_NUMBER);
 		client.start();
 	}
 
 	private void start() throws IOException {
-		Socket socket = new Socket("localhost", PORT_NUMBER);
-		InputStream socketInputStream = socket.getInputStream();
-		OutputStream socketOutputStream = socket.getOutputStream();
-		int readByte;
-		while ((readByte = System.in.read()) != -1) {
-			socketOutputStream.write(readByte);
-			int socketByte = socketInputStream.read();
-			System.out.write(socketByte);
+		try {
+			inputThread inputThread = new inputThread(socket);
+			outputThread outputThread = new outputThread(socket);
+			inputThread.start();
+			outputThread.start();
+			System.out.flush();
+			socket.shutdownOutput();
+			socket.close();
+		} catch (IOException e) {
+			System.out.println("thread creation failed.");
 		}
-		System.out.flush();
+	}	
+
+}
+
+class inputThread extends Thread{
+	 private Thread thread; 
+	 private Socket socket;
+
+	 inputThread(Socket s){
+			socket = s;
+	 }
+	 
+	 public void start() {
+		thread = new Thread(this);
+		thread.start();
+	 }
+
+	 public void run() { 
+		 try{
+			InputStream socketInputStream = socket.getInputStream();
+			int readByte;
+			while((readByte = System.in.read()) != -1){
+				int socketByte = socketInputStream.read();
+				System.out.write(socketByte);
+			}
+		 } catch (IOException e) {
+			 System.out.println("inputThread Exception");
+		 }
+	 }
+}
+
+class outputThread extends Thread{
+         private Thread thread;
+         private Socket socket;
+
+         outputThread(Socket s){
+			socket = s;
+         }
+
+        public void start() {
+		thread = new Thread(this);
+		thread.start();
 	}
+
+         public void run() {
+                 try{
+                        OutputStream socketOutputStream = socket.getOutputStream();
+			int readByte;
+			while((readByte = System.in.read()) != -1){
+				socketOutputStream.write(readByte);
+			}
+			socketOutputStream.flush();
+			socket.shutdownOutput();
+                 } catch (IOException e){
+                         System.out.println("outputThread Exception");
+                 }
+	 }
+
 }
